3f89cb42e638445cc9154d8e7b1f5b9347389646
pierre-geeraert <pierre.geeraert@viacesi.fr>
2016-11-14 14:55:49 +0100

    rajout addition
    rajout addition description

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int addition()
+{
+     return a+b;
+}
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+printf("%d\n", addition(1,4));
+return 0;
 }
